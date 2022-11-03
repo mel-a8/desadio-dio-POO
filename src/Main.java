4abe2fdf511ce09c4a6070b4d919f1104c40ebf5
Perkles <mel.al.gon10@gmail.com>
2022-11-03 18:41:37 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.awt.*;
 import java.time.LocalDate;
@@ -17,14 +16,48 @@ public class Main {
         curso2.setDescricao("descrição - Curso js");
         curso2.setCargaHorario(4);
 
+        Conteudo conteudo1 = new Curso();
+
+
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria java");
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+      /*  System.out.println(curso1);
         System.out.println(curso2);
+        System.out.println(mentoria1);
+
+       */
+
+        Bootcamp bootcamp   = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("descricao - Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devMelina = new Dev();
+        devMelina.setNome("Melina");
+        devMelina.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Melina: " + devMelina.getConteudosInscritos());
+        devMelina.progredir();
+        System.out.println("Conteudos Inscritos Melina: " + devMelina.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Melina: " + devMelina.getConteudosConcluidos());
+        System.out.println("------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos João: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos João: " + devJoao.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos João: " + devJoao.getConteudosInscritos());
+
     }
 
 }
